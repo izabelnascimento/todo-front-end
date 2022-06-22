ec75df6e6af6a1561b6f4f89ed2c7e48d8d75b70
Izabel Nascimento <izabelyale@hotmail.com>
2022-06-22 16:42:31 -0300

    Primeiro commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Todo } from './todo';
+import { TodoService } from './todo.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'front-end';
+
+export class AppComponent implements OnInit{
+
+  todos: Todo[] = [];
+
+  form: FormGroup = new FormGroup({
+    description: new FormControl('', [Validators.required, Validators.minLength(4)])
+  })
+
+  constructor(
+    private service: TodoService
+  ){}
+
+  ngOnInit(): void {
+  }
+  
+  list(){
+    this.service.list().subscribe(todoList => this.todos = todoList);
+  }
+
+  submit(){
+    console.log(this.form.value)
+    const todo: Todo = { ...this.form.value }
+    this.service
+      .save(todo)
+      .subscribe(savedTodo => {
+        this.todos.push(savedTodo);
+        this.form.reset();
+      });
+  }
+
+  delete(todo: Todo){
+    this.service.delete(todo.id).subscribe({
+      next: (response) => this.list()
+    })
+  }
+
+  done(todo: Todo){
+    this.service.done(todo.id).subscribe({
+      next: (response) => {
+        todo.done = response.done
+        todo.doneDate = response.doneDate
+      }
+    })
+  }
+
 }
